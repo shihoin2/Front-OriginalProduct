cd7561951d997bbedf50a2d12690cc74ba281984
shihoin2 <merysousi@gmail.comm>
2024-06-30 16:23:05 +0900

    ロゴ変更

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Page() {
     return (
         <Image
-            src="/MoguLogo.svg"
+            // src="/MoguLogo.svg"
+            src="/MoguSaLogo.png.svg"
             width={50}
             height={50}
             alt="WEAVE"
